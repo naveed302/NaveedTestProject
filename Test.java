aeb2b2db7dd2854f98dfc8cea15e4c93aede43ed
naveed302 <aakhan302@gmail.com>
2016-04-11 16:24:56 +0530

    modification in Test.java
    modification in Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,40 @@
 package naveed.practice.test;
 
+import java.util.StringTokenizer;
+
 public class Test {
 	public static void main(String[] args) {
-
+/*
 		StringBuffer fileName = new StringBuffer("D:CR_uploadedDoc/853/null");
 		System.out.println(fileName.delete(fileName.length() - 4,
 				fileName.length()));
+		
+		
+//====================================================================
+		String a = "Abraham";
+		String b = "Best Friend";
+		
+		a = String.format("%0$-" + b.length()+ "s   ", a);
+		b = String.format("%0$-" + b.length() + "s   ", b);
+		
+		System.out.println(a);
+		System.out.println(b);
+		*/
+		
+		//====================================================================
+		  String split_vls = "11. Welcome to java programming |1. Oops concepts |12. Encapsulation |3.Abstraction  |14.Inheritance  |5.Polymorphism";
+	         String[] Numeric_split = split_vls.split("\\|*(\\w+\\.)\\s*");
+	         for ( String pipes_Split : Numeric_split) {
+	       System.out.println(pipes_Split);
+	         }
+		StringTokenizer st= new StringTokenizer(split_vls, "\\|*(\\w+\\.)\\s*");
+		
+		while (st.hasMoreElements())
+			System.out.println(st.nextToken());
+			
+		
+		
+		
 
 	
 
